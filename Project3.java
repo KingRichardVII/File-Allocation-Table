6ed0ffd3772c9567b695ea4f6bc2220e7ccca684
Richard Phan <richard.phan626@gmail.com>
2025-11-22 20:55:24 -0800

    Add files via upload

--- Project3.java
+++ Project3.java
@@ -30,4 +30,59 @@ public class Project3 {
          table[i] = new Block();
       }
       //Main loop
+      int option = 0; // any value other than 4
+         while (option != 4) {
+         System.out.println();
+         System.out.println("1) Print the table and the file");
+         System.out.println("2) Add a block to the file");
+         System.out.println("3) Remove a block from the file");
+         System.out.println("4) Quit the program");
+         System.out.println("Enter selection: ");
+         if (input.hasNextInt()) {
+            option = input.nextInt();
+         } else {
+            option = 0; // any value other than 1,2,3,4
+         }
+         input.nextLine();//input flush
+         switch (option) {
+            case 1:
+               printTableAndFile();
+               break;
+            case 2:
+               addBlock();
+               break;
+            case 3:
+               removeBlock();
+               break;
+            case 4: 
+               System.out.println("Goodbye!");
+               break;
+            default:
+               System.out.println("Invalid option, try again.");
+          }// end of switch statement
+      } // end of main loop
 }//end of main method
+
+//Print table and File
+public static void printTableAndFile(){
+   //print the table view header row
+   System.out.println("Table View: ");
+   System.out.println("Index\tData\tNext");
+   //Print every block from the table as a row
+   for (int i = 0; i < table.length; i++) {
+      System.out.println(i + "\t" + table[i].getData() + "\t" + table[i].getNextIndex());
+      }
+}// end of Print Table and File
+
+//add block
+public static void addBlock() {
+   System.out.println("ADD BLOCK"); //FIXME: placeholder, remove later
+}// end of add block
+
+//remove block
+public static void removeBlock() {
+   System.out.print.ln("REMOVE BLOCK"); //FIXME: placeholder, remove later
+}// end of remove
+
+
+      
