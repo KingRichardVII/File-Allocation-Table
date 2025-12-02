e47e441e7b473fa8d7f6cd3925baced5b2213a9d
Richard Phan <richard.phan626@gmail.com>
2025-12-01 20:11:32 -0800

    Finished and debugged compile errors

--- Project3.java
+++ Project3.java
@@ -13,7 +13,7 @@ public class Project3 {
    //Main Method
    public static void main(String[] args){
       //Enter table size
-      System.out.println("Enter the table size: ");
+      System.out.println("Enter the table size:");
       if (!input.hasNextInt()){ //data type check
          System.out.println("Invalid input.");
          System.exit(0);
@@ -31,13 +31,13 @@ public class Project3 {
       }
       //Main loop
       int option = 0; // any value other than 4
-         while (option != 4) {
+      while (option != 4) {
          System.out.println();
          System.out.println("1) Print the table and the file");
          System.out.println("2) Add a block to the file");
          System.out.println("3) Remove a block from the file");
          System.out.println("4) Quit the program");
-         System.out.println("Enter selection: ");
+         System.out.println("Enter selection:");
          if (input.hasNextInt()) {
             option = input.nextInt();
          } else {
@@ -55,90 +55,135 @@ public class Project3 {
                removeBlock();
                break;
             case 4: 
-               System.out.println("Goodbye!");
+               System.out.println("Goodbye.");
                break;
             default:
                System.out.println("Invalid option, try again.");
           }// end of switch statement
       } // end of main loop
-}//end of main method
+   }//end of main method
 
-//Print table and File
-public static void printTableAndFile(){
-   //print the table view header row
-   System.out.println("Table View: ");
-   System.out.println("Index\tData\tNext");
-   //Print every block from the table as a row
-   for (int i = 0; i < table.length; i++) {
-      System.out.println(i + "\t" + table[i].getData() + "\t" + table[i].getNextIndex());
+   //Print table and File
+   public static void printTableAndFile(){
+      //print the table view header row
+      System.out.println("Table View:");
+      System.out.println("Index\tData\tNext");
+      //Print every block from the table as a row
+      for (int i = 0; i < table.length; i++) {
+         System.out.println(i + "\t" + table[i].getData() + "\t" + table[i].getNextIndex());
       }
-}// end of Print Table and File
 
-//add block
-public static void addBlock() {
-   //Enter the new block index
-   System.out.println("Enter an index for the block to add: ");
-   if (!input.hasNextInt()){//Data type check
-      System.out.println("Invalid input");
-      input.nextLine(); //input flush
-      return;
-   }
-   int newIndex = input.nextInt();
-   input.nextLine(); //input flushing
-   if (newIndex < 0 || newIndex >= table.length){//valid data check
-      System.out.println("Invalid block index");
-      return; 
-   }
-   //enter data for new block
-   System.out.println("Enter an unsigned integer of data: ");
-   if (!input.hasNextInt()) {
-      System.out.println("Invalid input");
-      input.nextLine();
-      return;
-   }
-   int newData = input.nextIn();
-   input.nextLine(); //input flush
-   if (newData < 0) { //valid data check
-      System.out.println("Invalid Data");
-      return;
-   }
-   //check value of head index
-   if (headIndex == -1) {
-      //assign new block to head of file
-      headIndex = newIndex;
-      table[newIndex].setData(newData);
-      System.out.println("Added first block at index " + newIndex + ".");
-   }else {
-      //iterate thru file and detect if new index is in use or not
-      //NOTE - new index will always be in use if table is full
+      //Print the file view (linked list starting at headIndex)
+      System.out.println("File View:");
+      System.out.println("Index\tData\tNext");
+      if (headIndex == -1) {
+         // empty file: just header
+         return;
+      }
       int currentIndex = headIndex;
-      while (currentIndex != newIndex) {
-         //the new index is not in use if the end of the file is reached
-         if (table[currentIndex].getNextIndex() == -1) {
-            //assign new block to end of file 
-            table[currentIndex].setNextIndex(newIndex);
-            table[newIndex].setData(newData);
-            System.out.println("Added new block at index " + newIndex + ".");
-            return;
-         }
+      while (currentIndex != -1) {
+         System.out.println(currentIndex + "\t" +
+                            table[currentIndex].getData() + "\t" +
+                            table[currentIndex].getNextIndex());
          currentIndex = table[currentIndex].getNextIndex();
       }
-      System.out.println("Duplicate block index detected");
+   }// end of Print Table and File
+
+   //add block
+   public static void addBlock() {
+      //Enter the new block index
+      System.out.println("Enter an index for the block to add:");
+      if (!input.hasNextInt()){//Data type check
+         System.out.println("Invalid input.");
+         input.nextLine(); //input flush
+         return;
+      }
+      int newIndex = input.nextInt();
+      input.nextLine(); //input flushing
+      if (newIndex < 0 || newIndex >= table.length){//valid data check
+         System.out.println("Invalid block index.");
+         return; 
+      }
+      //enter data for new block
+      System.out.println("Enter an unsigned integer of data:");
+      if (!input.hasNextInt()) {
+         System.out.println("Invalid input.");
+         input.nextLine();
+         return;
+      }
+      int newData = input.nextInt();
+      input.nextLine(); //input flush
+      if (newData < 0) { //valid data check
+         System.out.println("Invalid data.");
+         return;
+      }
+      //check value of head index
+      if (headIndex == -1) {
+         //assign new block to head of file
+         headIndex = newIndex;
+         table[newIndex].setData(newData);
+         System.out.println("Added first block at index " + newIndex + ".");
+      } else {
+         //iterate thru file and detect if new index is in use or not
+         //NOTE - new index will always be in use if table is full
+         int currentIndex = headIndex;
+         while (currentIndex != newIndex) {
+            //the new index is not in use if the end of the file is reached
+            if (table[currentIndex].getNextIndex() == -1) {
+               //assign new block to end of file 
+               table[currentIndex].setNextIndex(newIndex);
+               table[newIndex].setData(newData);
+               System.out.println("Added new block at index " + newIndex + ".");
+               return;
+            }
+            currentIndex = table[currentIndex].getNextIndex();
+         }
+         System.out.println("Duplicate block index detected.");
+      }
+   }// end of add block
+
+   //remove block
+   public static void removeBlock() {
+      //Enter removal index
+      System.out.println("Enter an index for the block to remove:");
+      if (!input.hasNextInt()) {//data type check
+         System.out.println("Invalid input.");
+         input.nextLine();//input flush
+         return;
+      }
+      int removalIndex = input.nextInt();
+      input.nextLine(); //input flush
+      if (removalIndex < 0 || removalIndex >= table.length) { //valid data check
+         System.out.println("Invalid block index.");
+         return;
+      }
+      //check value of head index
+      if (headIndex == removalIndex) {
+         //replace the head block with its next block
+         //NOTE - this next block can be either empty or not empty
+         int replacementIndex = table[removalIndex].getNextIndex();
+         table[removalIndex].setData(0);
+         table[removalIndex].setNextIndex(-1);
+         headIndex = replacementIndex;
+         System.out.println("Removed the block at head index " + removalIndex + ".");
+      } else {
+         //iterate through the file and search for the removal index
+         int currentIndex = headIndex;
+         while (currentIndex != -1) {
+            //check the value of the current block's next index
+            if (table[currentIndex].getNextIndex() == removalIndex) {
+               //Replace the current block's next block with own next block
+               //NOTE - this next block either empty or not empty
+               int replacementIndex = table[removalIndex].getNextIndex();
+               table[removalIndex].setData(0);
+               table[removalIndex].setNextIndex(-1);
+               table[currentIndex].setNextIndex(replacementIndex);
+               System.out.println("Removed the block at index " + removalIndex + ".");
+               return;
+            }
+            currentIndex = table[currentIndex].getNextIndex();
+         }
+         System.out.println("Unable to locate the block.");
+      }// end of remove  
    }
-}// end of add block
-
-//remove block
-public static void removeBlock() {
-   //Enter removal index
-      //make sure to check data type and input flush
-      
-   //check value of head index
-      //replace head block with its next block
-      
-      //iterate thru file and search for removal index
-         //check value of current block's next index
-         //replace current block's next block with its own block
-}// end of remove
-
-
-      
+}//end of class
